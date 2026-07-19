847f3eb6f09c75acc7391d8061159885188f9297
Allaminehafiz <allaminhafiz68@gmail.com>
2026-07-19 21:34:57 +0100

    first commit

--- src/main/java/com/sanctuary/sih/config/SecurityConfig.java
+++ src/main/java/com/sanctuary/sih/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "https://sanctuary-health.vercel.app"
+                "https://chu-health.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
